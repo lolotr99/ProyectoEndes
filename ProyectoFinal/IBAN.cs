f665e04975d6d4a8bbbb24df3b0330ebb809424f
lolotr99 <manolotoro80@gmail.com>
2019-05-24 13:46:16 +0200

    Arreglo fallo en el código y añado pruebas unitarias Con NUnit

--- ProyectoFinal/IBAN.cs
+++ ProyectoFinal/IBAN.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace dllISBN
+namespace ProyectoFinal
 {
     public class IBAN
     {
---
+++ ProyectoFinalTEST/IBANTEST.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ProyectoFinal;
+
+
+namespace ProyectoFinalTEST
+{
+    [TestFixture]
+    class IBANTEST
+    {
+        [TestCase]
+        public void DevolverErrorCuandoNoHayValor()
+        {
+            string valor = string.Empty;
+            IBAN iban = new IBAN();
+
+            bool resultado = iban.EsIBANvalido(valor);
+
+            Assert.AreEqual(false, resultado);
+        }
+
+        [TestCase]
+        public void DevuelveTrueCuandoEsValorCorrecto()
+        {
+            string valor = "ES6621000418401234567891";
+            IBAN iban = new IBAN();
+
+            bool resultado = iban.EsIBANvalido(valor);
+
+            Assert.AreEqual(true, resultado);
+        }
+
+
+
+    }
+}
